84c8bf2f29fdd72bf007f478b0590a8a3f67398b
anupsaw <anupsaw@gmail.com>
2015-06-04 00:31:28 -0400

    ExpressApp
    Express App

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var path = require('path')
+
+
+
+
+var app = express()
+
+app.use(express.static(__dirname + '/client'))
+
+app.get('/', function(req,res){
+	
+	res.sendFile(path.join(__dirname + '/client/index.html'))
+	
+	
+})
+
+
+
+
+
+app.listen('3000', function(req,res){
+	
+	
+	console.log("Server is listening on port 3000")
+	
+})
